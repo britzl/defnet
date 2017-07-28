0618f14eb0dcf0a49209dcabbb6efa7234ed7231
Björn Ritzl <bjorn.ritzl@king.com>
2017-07-28 10:19:16 +0200

    Updated the send queue to send all data on each call to update

--- defnet/tcp_send_queue.lua
+++ defnet/tcp_send_queue.lua
@@ -14,39 +14,40 @@ function M.create(client, chunk_size)
 	chunk_size = chunk_size or 10000
 
 	local instance = {}
-	
+
 	local queue = {}
-	
+
 	function instance.clear()
 		queue = {}
 	end
-	
+
 	function instance.add(data)
 		assert(data, "You must provide some data")
 		for i=1,#data,chunk_size do
 			table.insert(queue, { data = data:sub(i, i + chunk_size - 1), sent_index = 0 })
 		end
 	end
-	
-	function instance.send()
-		local first = queue[1]
-		if not first then
-			return
-		end
-		
-		local sent_index, err, sent_index_on_err = client:send(first.data, first.sent_index + 1, #first.data)
-		if err then
-			first.sent_index = sent_index_on_err
-			return false, err
-		end
 
-		first.sent_index = sent_index
-		if first.sent_index == #first.data then
-			table.remove(queue, 1)
+	function instance.send()
+		while true do
+			local first = queue[1]
+			if not first then
+				return true
+			end
+
+			local sent_index, err, sent_index_on_err = client:send(first.data, first.sent_index + 1, #first.data)
+			if err then
+				first.sent_index = sent_index_on_err
+				return false, err
+			end
+
+			first.sent_index = sent_index
+			if first.sent_index == #first.data then
+				table.remove(queue, 1)
+			end
 		end
-		return true
 	end
-	
+
 	return instance
 end
 
